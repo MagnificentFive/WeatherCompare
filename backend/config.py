3695839a89c2003491800c26ed59ff06b5116488
Ioann <ioann.chimrov@gmail.com>
2023-12-18 02:53:41 +0300

    Настроил основные ручки

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,7 @@
-open_weather_token = "your_token"
-tg_bot_token = 'your_token'
-news_api = 'your_token'
+import os
+
+DEBUG = os.getenv('DEBUG', '1') == '1'
+OPEN_WEATHER_TOKEN = os.getenv('OPEN_WEATHER_TOKEN', '')
+TG_BOT_TOKEN = os.getenv('TG_BOT_TOKEN', '')
+NEWS_TOKEN = os.getenv('NEWS_TOKEN', '')  # 'a128de079faa43e79575301d9c8a296d'
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///project.db' if DEBUG else '')
